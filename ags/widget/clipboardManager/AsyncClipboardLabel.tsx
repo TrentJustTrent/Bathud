ca2089aeefde7b1ca10e4fc5d8b3f1bb68eda374
John Oberhauser <j.git-global@obez.io>
2025-05-11 16:37:42 -0500

    clipboard improvements

---
+++ ags/widget/clipboardManager/AsyncClipboardLabel.tsx
@@ -0,0 +1,23 @@
+import {execAsync} from "astal/process";
+import {Variable} from "astal";
+import {insertNewlines} from "../utils/strings";
+
+export default function ({cliphistId}: {cliphistId: number}) {
+    const text = Variable("")
+    const label = <label
+        xalign={0}
+        wrap={true}
+        hexpand={true}
+        label={text()}/>
+
+    execAsync(["bash", "-c", `cliphist decode ${cliphistId}`]).catch((error) => {
+        console.log(error)
+    }).then((value) => {
+        if (typeof value !== "string") {
+            return
+        }
+        text.set(insertNewlines(value, 30))
+    })
+
+    return label
+}
