df8c700624523612c234da678fe88340b68bfef3
John Oberhauser <j.git-global@obez.io>
2025-09-05 19:55:55 -0500

    separate menu to launch on right click for shortcuts

--- ags/widget/barWidgets/Shortcut.tsx
+++ ags/widget/barWidgets/Shortcut.tsx
@@ -182,10 +182,17 @@ export default function (
                 },
                 onRightClick: ({self, x, y}) => {
                     createRoot((dispose) => {
+                        // @ts-ignore
+                        const clazz: string = variableConfig.barWidgets[`shortcut${shortcutNumber}`].class.get()
+                        let clients = hyprland
+                            .clients
+                            .filter((it) => it.class === clazz)
+
                         // 1) Build a menu model
                         const model = new Gio.Menu();
-                        model.append("New Window",         "main.new-window");
-                        model.append("Quit",               "main.quit");
+                        model.append("Launch", "main.launch")
+                        model.append("New Window", "main.new-window");
+                        model.append("Quit", "main.quit");
 
                         const pop = Gtk.PopoverMenu.new_from_model(model);
                         pop.set_has_arrow(false);
@@ -193,37 +200,54 @@ export default function (
                         // 2) Provide the actions on the popover (prefix "dock")
                         const group = new Gio.SimpleActionGroup();
 
-                        const newWindowAction = new Gio.SimpleAction({ name: "new-window" });
-                        newWindowAction.connect("activate", () => {
-                            pop.popdown();
-                            // @ts-ignore
-                            const command: string = variableConfig.barWidgets[`shortcut${shortcutNumber}`].newWindow.get()
-                            if (command !== "") {
-                                launchApp([command])
-                            }
-                        });
-                        group.add_action(newWindowAction);
+                        if (clients.length === 0) {
+                            // No opened clients
+                            const newWindowAction = new Gio.SimpleAction({ name: "launch" });
+                            newWindowAction.connect("activate", () => {
+                                pop.popdown();
+                                // @ts-ignore
+                                const command: string = variableConfig.barWidgets[`shortcut${shortcutNumber}`].launch.get()
+                                if (command !== "") {
+                                    launchApp([command])
+                                }
+                            });
+                            group.add_action(newWindowAction);
 
-                        const quitAction = new Gio.SimpleAction({ name: "quit" });
-                        quitAction.connect("activate", () => {
-                            pop.popdown();
-                            // @ts-ignore
-                            const clazz: string = variableConfig.barWidgets[`shortcut${shortcutNumber}`].class.get()
-                            hyprland.clients.filter((it) => it.class === clazz).forEach((it) => it.kill())
-                        });
-                        group.add_action(quitAction);
+                            const root = new Gio.Menu();
+                            root.append("Launch", "main.launch")
+                            pop.set_menu_model(root);
+                        } else {
+                            // Has opened clients
+                            const newWindowAction = new Gio.SimpleAction({name: "new-window"});
+                            newWindowAction.connect("activate", () => {
+                                pop.popdown();
+                                // @ts-ignore
+                                const command: string = variableConfig.barWidgets[`shortcut${shortcutNumber}`].newWindow.get()
+                                if (command !== "") {
+                                    launchApp([command])
+                                }
+                            });
+                            group.add_action(newWindowAction);
+
+                            const quitAction = new Gio.SimpleAction({name: "quit"});
+                            quitAction.connect("activate", () => {
+                                pop.popdown();
+                                // @ts-ignore
+                                const clazz: string = variableConfig.barWidgets[`shortcut${shortcutNumber}`].class.get()
+                                hyprland.clients.filter((it) => it.class === clazz).forEach((it) => it.kill())
+                            });
+                            group.add_action(quitAction);
+
+                            const root = new Gio.Menu();
+                            root.append("New Window", "main.new-window");
+                            root.append("Quit", "main.quit");
+                            pop.set_menu_model(root);
+                        }
 
                         // Attach the group to the popover under the "main" prefix
                         // (GTK will search the popover, then its parents, for "main.*")
                         pop.insert_action_group?.("main", group);
 
-                        // 3) create menu
-                        const root = new Gio.Menu();
-                        root.append("New Window", "main.new-window");
-                        root.append("Quit", "main.quit");
-                        pop.set_menu_model(root);
-
-                        // 4) Parent & position
                         pop.set_parent(self);
 
                         const rect = new Gdk.Rectangle({ x: Math.round(x), y: Math.round(y), width: 1, height: 1 });
