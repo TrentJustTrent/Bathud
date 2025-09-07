2d58387492896bbc28df9806507ec509e76fc3ac
John Oberhauser <j.git-global@obez.io>
2025-09-07 10:41:36 -0500

    improving shortcut left click logic

--- ags/widget/barWidgets/Shortcut.tsx
+++ ags/widget/barWidgets/Shortcut.tsx
@@ -6,9 +6,7 @@ import {getHPadding, getVPadding} from "./BarWidgets";
 import {Gdk, Gtk} from "ags/gtk4";
 import AstalHyprland from "gi://AstalHyprland?version=0.1";
 import {launchApp} from "../utils/launch";
-import {timeout, Timer} from "ags/time";
 import Gio from "gi://Gio?version=2.0";
-import GLib from "gi://GLib?version=2.0";
 
 function getIndicatorHAlign(bar: Bar) {
     switch (bar) {
@@ -63,9 +61,6 @@ export default function (
         return focused?.class === clazz
     })
 
-    let cycleClientsTimer: Timer | null = null
-    let lastClientFocusId: number | null = null
-
     return <overlay
         $={(self) => {
             self.add_overlay(
@@ -107,7 +102,6 @@ export default function (
                         .sort((a, b) => a.focusHistoryId - b.focusHistoryId)
 
                     console.log(`clients: ${clients.map((it) => it.focusHistoryId)}`)
-                    console.log(`lastClientFocusId: ${lastClientFocusId}`)
 
                     if (clients.length === 0) {
                         // If there are no clients open, launch one
@@ -118,70 +112,31 @@ export default function (
                         }
                     } else {
 
-                        // Cycle through clients if we focused recently
-                        if (cycleClientsTimer !== null && lastClientFocusId !== null) {
-                            cycleClientsTimer.cancel()
-
-                            clients = clients
-                                .filter((it) => it.focusHistoryId > lastClientFocusId!)
-
-                            if (clients.length === 0) {
-                                clients = hyprland
-                                    .clients
-                                    .filter((it) => it.class === clazz)
-                                    .sort((a, b) => a.focusHistoryId - b.focusHistoryId)
-                            }
-
-                            const lastFocusedClient = clients[0]
-                            const workspace = lastFocusedClient.get_workspace()
-                            if (workspace.id !== hyprland.get_focused_workspace().id) {
-                                workspace.focus()
-                            }
-                            lastFocusedClient.focus()
-                            lastClientFocusId = lastFocusedClient.focusHistoryId
-                            if (clients.length > 1) {
-                                cycleClientsTimer = timeout(5000, () => {
-                                    cycleClientsTimer = null
-                                })
-                            }
-
-                            return
-                        }
-
-                        // If we are not cycling yet
-                        // Focus on the most recently created client in the current workspace
+                        const currentFocusedClient = hyprland.get_focused_client()
                         const focusedWorkspace = hyprland.get_focused_workspace()
-                        let clientsOnFocusedWorkspace = focusedWorkspace
-                            .clients
-                            .filter((it) => it.class === clazz)
-                            .sort((a, b) => a.focusHistoryId - b.focusHistoryId)
 
-                        if (clientsOnFocusedWorkspace.length !== 0) {
-                            const lastFocusedClient = clients[0]
-                            lastFocusedClient.focus()
-                            lastClientFocusId = lastFocusedClient.focusHistoryId
-                            if (clients.length > 1) {
-                                cycleClientsTimer = timeout(5000, () => {
-                                    cycleClientsTimer = null
-                                })
+                        // If we are already focused on the class, focus the next client
+                        if (currentFocusedClient.class === clazz) {
+                            const nextClients = clients
+                                .filter((it) => it.focusHistoryId > currentFocusedClient.focusHistoryId)
+
+                            const clientToFocus = nextClients.length === 0 ? clients[0] : nextClients[0]
+                            const clientToFocusWorkspace = clientToFocus.workspace
+                            if (clientToFocusWorkspace.id !== focusedWorkspace.id) {
+                                clientToFocus.workspace.focus()
                             }
+                            clientToFocus.focus()
                             return
                         }
 
-                        // If this workspace has no clients
-                        // Focus on the most recently created client
-                        const lastFocusedClient = clients[0]
-                        const workspace = lastFocusedClient.get_workspace()
-                        if (workspace.id !== hyprland.get_focused_workspace().id) {
-                            workspace.focus()
+                        // Focus on a client in this workspace if one exists, otherwise a client elsewhere
+                        const clientsOnFocusedWorkspace = clients.filter((it) => it.workspace === focusedWorkspace)
+                        const clientToFocus = clientsOnFocusedWorkspace.length === 0 ? clients[0] : clientsOnFocusedWorkspace[0]
+                        const clientToFocusWorkspace = clientToFocus.workspace
+                        if (clientToFocusWorkspace.id !== focusedWorkspace.id) {
+                            clientToFocus.workspace.focus()
                         }
-                        lastFocusedClient.focus()
-                        lastClientFocusId = lastFocusedClient.focusHistoryId
-                        if (clients.length > 1) {
-                            cycleClientsTimer = timeout(5000, () => {
-                                cycleClientsTimer = null
-                            })
-                        }
+                        clientToFocus.focus()
                     }
                 },
                 onMiddleClick: () => {
