0a2d2e224ffe6467f829fa10b887e095b1ed2ded
batman <batman@wayneenterprises.com>
2025-12-14 16:38:17 -0600

    Nix packages

--- package.nix
+++ package.nix
@@ -20,6 +20,16 @@
             astal4
             glib
             gjs
+            Apps
+            Battery
+            Bluetooth
+            Cava
+            Hyprland
+            Mpris
+            Network
+            Notifd
+            PowerProfiles
+            Tray
             pipewire
             networkmanager
             bluez
